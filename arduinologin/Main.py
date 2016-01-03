702f079a703196051d1685d20426b25bffdd0237
Eduardo Ugolini <Eduardo Ugolini>
2016-01-03 01:00:34 -0200

    Now it works on Windows

--- arduinologin/Main.py
+++ arduinologin/Main.py
@@ -1,33 +1,41 @@
+import win32com.client as OSsender
+import sys
 import serial
 import gc
 import time
-import os
-from subprocess import Popen, PIPE
 
-passwordSequence = ''' 
-key Return
-key p
-key a
-key s
-key s
-key w
-key o
-key r
-key d
-key Return
-'''
+password = 'YOUR PASSWORD HERE!!!' 
 
 def SendPassword():
-    p = Popen(['xte'], stdin=PIPE)
-    p.communicate(input=passwordSequence)
+    keySender = OSsender.Dispatch("WScript.Shell")
+    keySender.SendKeys('{ENTER}')
+    keySender.SendKeys(password)
+    keySender.SendKeys('{ENTER}')
 
+# Function adapted from: http://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python
 def GetSerialPorts():
-    return "/dev/"+os.popen("dmesg | egrep ttyACM | cut -f3 -d: | tail -n1").read().strip()
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
 
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            return port
+        except (OSError, serial.SerialException):
+            pass
+        
 def MainFunction():
     gc.enable()
     while True:
-        arduinoSerial = serial.Serial(GetSerialPorts(), 9600) #/dev/ttyACM0 is the port where arduino is connected
+        arduinoSerial = serial.Serial(GetSerialPorts(), 9600)
         serialRead = arduinoSerial.read(4)
 
         if serialRead == "True" :
@@ -39,5 +47,3 @@ def MainFunction():
         
 if __name__ == '__main__':       
     MainFunction()
-
-
